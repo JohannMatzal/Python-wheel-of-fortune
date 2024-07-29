6947539c77e6f7b7301be2467ef3b7989cfcd6a6
Johann Matzal <78445094+JohannMatzal@users.noreply.github.com>
2024-07-29 14:13:59 -0400

    Update solo_wof.py

--- solo_wof.py
+++ solo_wof.py
@@ -1,10 +1,3 @@
-###############################################################################
-# Author: Johann Matzal
-# Date: 4/28/2021
-# Description: This program simulates a wheel of fortune. There are 4 rounds and
-# over 40 unique phrases. Each round you will have the option to spin the wheel,
-# buy a vowel, guess the phrase, or quit the game.
-###############################################################################
 import random as r
 import math as m
 
